573689b34070c73c7feed7dbcd71da0b7e6ec971
Kenytt Avery <kavery@fullerton.edu>
2018-10-14 22:23:08 -0700

    Initial commit

---
+++ app/src/main/java/edu/fullerton/ecs/cpsc411/kotlincustomlayoutlistview/MainActivity.kt
@@ -0,0 +1,57 @@
+package edu.fullerton.ecs.cpsc411.kotlincustomlayoutlistview
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item.view.*
+
+data class Person(val name: String, val email: String)
+
+// Courtesy of <https://www.fakenamegenerator.com/>
+val people = arrayOf(
+    Person("Victor S. Balch", "VictorSBalch@rhyta.com"),
+    Person("Robert S. Morgan", "RobertSMorgan@jourrapide.com "),
+    Person("Corine J. Day", "CorineJDay@armyspy.com "),
+    Person("David P. Craig", "DavidPCraig@jourrapide.com "),
+    Person("Stella R. Kingsley", "StellaRKingsley@dayrep.com "),
+    Person("Leonor C. Coleman", "LeonorCColeman@jourrapide.com"),
+    Person("Gregory B. Peck", "GregoryBPeck@jourrapide.com"),
+    Person("Alex R. Brady", "AlexRBrady@dayrep.com"),
+    Person("Patrice S. Ponce", "PatriceSPonce@rhyta.com"),
+    Person("Eva D. Simmons", "EvaDSimmons@jourrapide.com"),
+    Person("David V. Higgins", "DavidVHiggins@rhyta.com"),
+    Person("Beatrice J. Hernadez", "BeatriceJHernadez@dayrep.com")
+)
+
+class PersonAdapter(context: Context) : ArrayAdapter<Person>(context, R.layout.item, R.id.nameTextView, people) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val item = super.getView(position, convertView, parent)
+
+        item.nameTextView.text = people[position].name
+        item.emailTextView.text = people[position].email
+
+        return item
+    }
+}
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val adapter = PersonAdapter(this)
+
+        listView.adapter = adapter
+        listView.setOnItemClickListener { _, _, position, _ ->
+            textView.text = "\"${people[position].name}\" <${people[position].email}>"
+        }
+
+    }
+}
